83b65c23c71841d5a849b6f25573324c5c6eccb7
qingsheng yang <yangqingsheng12@outlook.com>
2020-09-11 13:24:49 +0800

    isAnagram_map

--- Practice/LeetCode/244 isAnagram(easy)/isAnagram.cpp
+++ Practice/LeetCode/244 isAnagram(easy)/isAnagram.cpp
@@ -8,3 +8,34 @@ public:
         else return false;
     }
 };
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        //1.使用数组映射
+        if(s.size() != t.size())return false;
+        
+        int record[26] = {0};
+        for(int i = 0; i < s.size(); ++i){
+            record[s[i] - 'a']++;
+        }
+        for(int i = 0; i < t.size(); ++i){
+            if((--record[t[i] - 'a']) == -1)return false; //注意这里的巧妙判断
+        }
+        return true;
+    }
+};
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        //1.使用数组映射
+        if(s.size() != t.size())return false;
+        
+        unordered_map<char, int> map;
+        for(char c : s)map[c] ++;
+        for(char c : t)
+            if(-- map[c] == -1)return false;
+        return true;
+    }
+};
