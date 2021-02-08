8f82ade974444866b345a469085057a7b0fd9811
yangqings <1135802814@qq.com>
2021-02-09 00:57:21 +0800

    Update Leetcode mergeKLists

--- Practice_Code/LeetCode/23.MergeKSortedLists(hard)/mergeKLists.cpp
+++ Practice_Code/LeetCode/23.MergeKSortedLists(hard)/mergeKLists.cpp
@@ -1,4 +1,28 @@
 /**
+ * @brief 两个等长有序链表合并
+ *        思路，原地直接合并，时间复杂度O(n),空间复杂度O(1)
+ * @param a 
+ * @param b 
+ * @return ListNode* 
+ */
+
+ListNode* mergeTwoLists(ListNode *a, ListNode *b) {
+    if ((!a) || (!b)) return a ? a : b;
+    ListNode head, *tail = &head, *aPtr = a, *bPtr = b;
+    while (aPtr && bPtr) {
+        if (aPtr->val < bPtr->val) {
+            tail->next = aPtr; aPtr = aPtr->next;
+        } else {
+            tail->next = bPtr; bPtr = bPtr->next;
+        }
+        tail = tail->next;
+    }
+    tail->next = (aPtr ? aPtr : bPtr);
+    return head.next;
+}
+
+
+/**
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
@@ -10,9 +34,9 @@
  */
 
  /**
-  * @brief k个链表合并成有序链表
+  * @brief k个有序链表合并成一个有序链表
   *        思路，将k个链表节点用优先队列存放，然后从优先队列逐个取出，构成有序链表
-
+  *        优先队列，获得最大（最小）元素时间复杂度O(1),插入或者删除元素O(logn)
   */
 class Solution {
 public:
