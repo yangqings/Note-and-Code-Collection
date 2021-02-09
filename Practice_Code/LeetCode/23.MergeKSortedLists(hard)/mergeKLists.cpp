c9f3b1627ef132b611b15a7ca5989c2084e0a59e
yangqings <1135802814@qq.com>
2021-02-09 12:46:11 +0800

    Update Leetcode mergeKLists

--- Practice_Code/LeetCode/23.MergeKSortedLists(hard)/mergeKLists.cpp
+++ Practice_Code/LeetCode/23.MergeKSortedLists(hard)/mergeKLists.cpp
@@ -1,28 +1,4 @@
 /**
- * @brief 两个等长有序链表合并
- *        思路，原地直接合并，时间复杂度O(n),空间复杂度O(1)
- * @param a 
- * @param b 
- * @return ListNode* 
- */
-
-ListNode* mergeTwoLists(ListNode *a, ListNode *b) {
-    if ((!a) || (!b)) return a ? a : b;
-    ListNode head, *tail = &head, *aPtr = a, *bPtr = b;
-    while (aPtr && bPtr) {
-        if (aPtr->val < bPtr->val) {
-            tail->next = aPtr; aPtr = aPtr->next;
-        } else {
-            tail->next = bPtr; bPtr = bPtr->next;
-        }
-        tail = tail->next;
-    }
-    tail->next = (aPtr ? aPtr : bPtr);
-    return head.next;
-}
-
-
-/**
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
@@ -32,6 +8,73 @@ ListNode* mergeTwoLists(ListNode *a, ListNode *b) {
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+
+/**
+ * @brief 解法一：直接思路，循环顺序合并两个链表，直到处理完所有
+ *               时间复杂度：O(n)
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode *a, ListNode *b) {
+        if ((!a) || (!b)) return a ? a : b;
+        ListNode head, *tail = &head, *aPtr = a, *bPtr = b;
+        while (aPtr && bPtr) {
+            if (aPtr->val < bPtr->val) {
+                tail->next = aPtr; aPtr = aPtr->next;
+            } else {
+                tail->next = bPtr; bPtr = bPtr->next;
+            }
+            tail = tail->next;
+        }
+        tail->next = (aPtr ? aPtr : bPtr);
+        return head.next;
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        ListNode* ans = nullptr;
+        for(auto list : lists){
+            ans = mergeTwoLists(ans,list);
+        }
+        return ans;
+    }
+};
+
+
+/**
+ * @brief 解法二：并归优化
+ *               渐进时间复杂度为 O(Kn*logK)
+ *               由于使用递归，空间复杂度O(logk)
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode *a, ListNode *b) {
+        if ((!a) || (!b)) return a ? a : b;
+        ListNode head, *tail = &head, *aPtr = a, *bPtr = b;
+        while (aPtr && bPtr) {
+            if (aPtr->val < bPtr->val) {
+                tail->next = aPtr; aPtr = aPtr->next;
+            } else {
+                tail->next = bPtr; bPtr = bPtr->next;
+            }
+            tail = tail->next;
+        }
+        tail->next = (aPtr ? aPtr : bPtr);
+        return head.next;
+    }
+
+    ListNode* merge(vector<ListNode*> lists, int l, int r){
+        if(l == r) return lists[l];
+        if(l > r) return nullptr;
+        int mid = (l + r) >> 1;
+        return mergeTwoLists(merge(lists, l, mid), merge(lists, mid + 1, r));
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        return merge(lists, 0, lists.size() - 1);
+    }
+};
+
 
  /**
   * @brief k个有序链表合并成一个有序链表
