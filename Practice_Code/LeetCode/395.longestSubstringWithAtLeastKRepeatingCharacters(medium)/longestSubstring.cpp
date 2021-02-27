2e1c4565de9485313b137d6730ef1ab6522d3bcb
yangqings <1135802814@qq.com>
2021-02-27 20:54:13 +0800

    Add Leetcode longestSubstring

---
+++ Practice_Code/LeetCode/395.longestSubstringWithAtLeastKRepeatingCharacters(medium)/longestSubstring.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int longestSubstring(string s, int k) {
+        if (s.size() < k) return 0;
+        unordered_set<char> chars(s.begin(), s.end());
+        unordered_map<char, int> counter;
+        for (char c : s) 
+            counter[c] ++;
+        for (char c : chars) {
+            vector<string> t; 
+            if (counter[c] < k) {
+                split(s, t, c);
+                int res = 0;
+                for (string tn : t) {
+                    res = max(res, longestSubstring(tn, k));
+                }
+                return res;
+            }
+        }
+        return s.size();
+    }
+    void split(const string& s, vector<string>& sv,const char flag = ' ') {
+    sv.clear();
+    istringstream iss(s);
+    string temp;
+
+    while (getline(iss, temp, flag)) {
+        sv.push_back(temp);
+    }
+}
+};
