ea8dddb863bcecda29591217bd6f6e66bfef8d0f
yangqingsheng <yangqingsheng12@outlook.com>
2021-03-01 17:30:02 +0800

    Update Leetcode longestSubstring

--- Practice_Code/LeetCode/395.longestSubstringWithAtLeastKRepeatingCharacters(medium)/longestSubstring.cpp
+++ Practice_Code/LeetCode/395.longestSubstringWithAtLeastKRepeatingCharacters(medium)/longestSubstring.cpp
@@ -29,3 +29,50 @@ public:
     }
 }
 };
+
+
+class Solution{
+public:
+    int dfs(string& s, int l, int r. int k){
+        if( r - l + 1 < k)return 0;
+        vector<int> cnt(26, 0);
+
+        for(int i = l; i <= r; ++i){  //统计字母出现次数
+            cnt[ s[i] - 'a' ] ++;
+        }
+
+        char split = 0;
+        for(int i = 0; i < 26; ++i){  //找到分割字母
+            if(cnt[i] > 0 && cnt[i] < k){
+                split = s[i] + 'a';
+                break;
+            }
+        }
+
+        if(split == 0) return r - l + 1; 
+
+        int i = l;
+        int ret = 0;
+        while( i <= r){
+            while(i <= r && s[i] == split){ //找 left
+                ++i;
+            }
+            if(l > r)break;
+
+            int start = i; 
+            while(i <= r && s[i] != split){ //找 right
+                ++i;
+            }
+
+            int length = dfs(s, start, i - 1; k);
+            ret = max(ret, length);
+        }
+
+        return ret;
+    }
+
+    int longestSubstring(string s, int k){
+        int n = string.length();
+        return dfs(s, 0, n - 1, k);
+    }
+}   
